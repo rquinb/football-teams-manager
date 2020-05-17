47265b28a9c72a6f887a81f621c99870d938e1fc
matias.quintana <matias.quintana.barth@gmail.com>
2020-05-17 00:03:31 -0300

    Adds library for drawing football pitch and placing players on it, creates sample html for that page and adds route to flask

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def main_page():
     return flask.render_template("index.html")
 
+@app.route("/creador-de-equipos/")
+def teams_creator_page():
+    return flask.render_template("teams_creator.html")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- static/script.js
+++ static/script.js
@@ -12,4 +12,24 @@ $("#nuevo-jugador").on("click",function(){
     <td>${stamina}</td>
     <td>${tecnica}</td>
     </tr>`);
-})
+});
+
+$("#cancha").soccerfield([{name: 'Matias', position: 'C_GK'}],{
+    field: {
+      width: "500px",
+      height: "400px",
+      img: '../static/Soccer-Field-Players-Positions/src/img/soccerfield_green.png',
+      startHidden: false,
+      animate: false,
+      fadeTime: 1000,
+      autoReveal:true
+    },
+    players: {
+        font_size: 10,
+        reveal: true,
+        sim: true,
+        timeout: 1000,
+        fadeTime: 1000,
+        img: false
+    }
+});
