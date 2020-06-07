cb1b445c7e48ea60139be8a5a707b5db1b2e99df
matias.quintana <matias.quintana.barth@gmail.com>
2020-06-07 20:15:39 -0300

    Fixes on "ingresar()" function

--- static/scriptshomepage.js
+++ static/scriptshomepage.js
@@ -1,16 +1,16 @@
 function ingresar(){
     let usuario= document.getElementById("Usuario").value;
     let contraseña= document.getElementById("Contraseña").value;
-    let datosUsuario= JSON.parse(localStorage.getItem("datosusuarios")); 
-    for (let i=0; i<datosUsuario["datos"].lenght; i++){
-        if(usuario==datosUsuario [i]["nuevoUsuario"]){
-            if(contraseña==datosUsuario [i]["contraseña"]){
-                    window.location.href="http://localhost:5000/administrador-de-equipos"
+    let datosUsuario= JSON.parse(localStorage.getItem("datosusuarios"))['datos']; 
+    for (let i=0; i<datosUsuario.length; i++){
+        if(usuario==datosUsuario[i]["nuevoUsuario"]){
+            if(contraseña==datosUsuario[i]["contraseña"]){
+                    window.location.href="http://localhost:5000/administrador-de-jugadores"
                 } else {
-                    alert("Contraseña incorrecta");//sino sde la comparación de las contraseñas
+                    alert("Contraseña incorrecta");
+                    return;//sino sde la comparación de las contraseñas
                 }
-            } else {
-                alert("Usuario no existente")//sino de la comparación de los usuarios
-            }
+             }
         }
-}
+        alert("Usuario no existente");
+    }
