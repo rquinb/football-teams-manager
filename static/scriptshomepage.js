186620d40cf259938cadc4b597138fa96caa5151
Josephoartigasia <rgcuello19@gmail.com>
2020-06-07 19:58:47 -0300

    Update scriptshomepage.js

--- static/scriptshomepage.js
+++ static/scriptshomepage.js
@@ -1,13 +1,11 @@
 function ingresar(){
-    let usuario= document.getElementById("Usuario").value;//obtener dato ingresado en el input de usuario
-    let contraseña= document.getElementById("Contraseña").value;//obtener dato ingresado en el input contraseña
-    let datosUsuario= [JSON.parse(localStorage.getItem("nuevoUsuario"))];//obtener dato guardado en el localstorage del usuario 
-    let datosContraseña= [JSON.parse(localStorage.getItem("contraseña"))];//obtener dato guardado en el localstorage de contraseña
-    for (let i=0; i<datosUsuario["nuevoUsuario"].lenght; i++){//reccorrer datosUsuario para ver los usuarios guardados
-        if(usuario==datosUsuario["nuevoUsuario"]){//if para comparar el usuario ingresado en la homepage y los usuarios guardados
-            for (let x=0; x<datosContraseña["contraseña"].lenght; x++)//recorrer contraseñas guardadas
-                if(contraseña==datosContraseña["contraseña"]){//if para comparar las contraseña con la guardada
-                    window.location.href="https://www.youtube.com/watch?v=nhPaWIeULKk"//si coinciden las dos pasar a la ventana de creación de equipos
+    let usuario= document.getElementById("Usuario").value;
+    let contraseña= document.getElementById("Contraseña").value;
+    let datosUsuario= JSON.parse(localStorage.getItem("datosusuarios")); 
+    for (let i=0; i<datosUsuario["datos"].lenght; i++){
+        if(usuario==datosUsuario [i]["nuevoUsuario"]){
+            if(contraseña==datosUsuario [i]["contraseña"]){
+                    window.location.href="http://localhost:5000/administrador-de-equipos"
                 } else {
                     alert("Contraseña incorrecta");//sino sde la comparación de las contraseñas
                 }
@@ -15,4 +13,4 @@ function ingresar(){
                 alert("Usuario no existente")//sino de la comparación de los usuarios
             }
         }
-    }   
+}
